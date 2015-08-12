aa4902723fbdafb24c75b3c356d1a39e31a5ab2b
KIMURA Tetsuro <haribote.nobody@gmail.com>
2015-08-12 13:04:40 +0900

    eslint configs to silent mode.

--- bin/watch-js.sh
+++ bin/watch-js.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-parallelshell "watchify ./src/assets/js/main.js -t babelify -o 'exorcist ./dest/assets/js/bundle.js.map > ./dest/assets/js/bundle.js' -v -d" "watch 'npm run lint:js' ./src/assets/js/"
+parallelshell "watchify ./src/assets/js/main.js -t babelify -o 'exorcist ./dest/assets/js/bundle.js.map > ./dest/assets/js/bundle.js' -v -d" "watch 'npm run -s lint:js' ./src/assets/js/"
