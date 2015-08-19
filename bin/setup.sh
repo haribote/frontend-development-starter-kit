15a89f66e1653fb34216bf07a35b5bc37af8b55c
KIMURA Tetsuro <haribote.nobody@gmail.com>
2015-08-19 23:56:07 +0900

    remove bootstrap

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bower install && cp -v ./bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss ./src/assets/css/_bootstrap.scss && cp -v ./bower_components/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss ./src/assets/css/_variables.scss && gulp build:pre && mkdir -v -p ./dest/assets/js
+bower install && gulp build:pre && mkdir -v -p ./dest/assets/js
